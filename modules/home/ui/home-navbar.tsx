3e4b3cffee84d34cec943e4197c61f1f1fffe88a
Euger Bonete <azkriven17@gmail.com>
2025-08-07 13:48:08 +0800

    removed bg opacity in home navbar

--- modules/home/ui/home-navbar.tsx
+++ modules/home/ui/home-navbar.tsx
@@ -101,7 +101,7 @@ export const Navbar = ({
     <section
       className={cn(
         poppins.className,
-        "py-2 px-4 md:px-6 block sticky top-0 z-50 bg-background/80 backdrop-blur-md"
+        "py-2 px-4 md:px-6 block sticky top-0 z-50 bg-background backdrop-blur-md"
       )}
     >
       <div className="container">
